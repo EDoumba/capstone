954839268031001aa39168fa2ddf9844b84e1295
EDoumba <ernest.doumba@yahoo.fr>
2025-09-03 08:48:27 -0700

    capstone

--- .history/vet-app/src/app/core/interceptors/auth.interceptor_20250903084617.ts
+++ .history/vet-app/src/app/core/interceptors/auth.interceptor_20250903084617.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
-import {HttpRequest, HttpHandler,HttpEvent,HttpInterceptor} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
+import {  HttpRequest,HttpHandler,HttpEvent,HttpInterceptor, HttpErrorResponse} from '@angular/common/http';
+import { catchError, filter, take, switchMap } from 'rxjs/operators';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(private authService: AuthService) {} // ✅ used here
+  //constructor() {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     // Add auth token for API requests
